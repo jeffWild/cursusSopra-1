d4ad4d28b0412744b52a3098f7099f2f8942c9e1
vincent courtalon <vincent.courtalon@gmail.com>
2016-10-04 09:22:43 +0200

    ajout de la fonction fait calcul

--- gitmavenmodule/premierprojetGit/src/premierprojetGit/Program.java
+++ gitmavenmodule/premierprojetGit/src/premierprojetGit/Program.java
@@ -5,6 +5,11 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("autre essai");
+		System.out.println(faitcalcul(9));
+	}
+	
+	public static int faitcalcul(int value) {
+		return (int)(value / 2.0 + 0.5);
 	}
 
 }
