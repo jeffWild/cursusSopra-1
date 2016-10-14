538da649c95d5fcb9dced150925b75711ab373b7
vincent courtalon <vincent.courtalon@gmail.com>
2016-10-14 12:02:13 +0200

    ajout liste des produits dans app web spring web

--- modulespring/firstSpringWebForm/src/main/java/com/courtalon/firstSpringWebForm/servlets/IndexServlet.java
+++ modulespring/firstSpringWebForm/src/main/java/com/courtalon/firstSpringWebForm/servlets/IndexServlet.java
@@ -1,12 +1,16 @@
 package com.courtalon.firstSpringWebForm.servlets;
 
 import java.util.Date;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.courtalon.firstSpringWebForm.beans.Message;
+import com.courtalon.firstSpringWebForm.metier.Produit;
+import com.courtalon.firstSpringWebForm.utils.ProduitDAO;
 
 import java.io.*;
 import javax.servlet.*;
@@ -17,12 +21,26 @@ public class IndexServlet extends SpringBaseServlet {
 	private static Logger log = LogManager.getLogger(IndexServlet.class); 
 	private static final long serialVersionUID = 1L;
 
+	private ProduitDAO produitDAO;
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		this.produitDAO = ctx.getBean("produitDAO", ProduitDAO.class);
+	}
 
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException, ServletException {
 
-      response.setContentType("text/html");
+	  List<Produit> produits = produitDAO.findAll();
+	  request.setAttribute("produits", produits);
+	  getServletContext().getRequestDispatcher("/produits.jsp")
+	  					.forward(request, response);
+	  
+		
+		
+   /*   response.setContentType("text/html");
 
       PrintWriter out = response.getWriter();
       
@@ -36,7 +54,7 @@ public class IndexServlet extends SpringBaseServlet {
       out.println("<body>");
       out.println("<h1>" + msg.getCorps() + "</h1>");
       out.println("</body>");
-      out.println("</html>");
+      out.println("</html>");*/
    }
 
 }
---
+++ modulespring/firstSpringWebForm/src/main/java/com/courtalon/firstSpringWebForm/utils/ProduitDAO.java
@@ -0,0 +1,66 @@
+package com.courtalon.firstSpringWebForm.utils;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.courtalon.firstSpringWebForm.metier.Produit;
+
+
+
+public class ProduitDAO {
+	// cette objet/classe sert a faire la correspondance entre
+	// une ligne renvoyée par une requette et un objet Produit
+	// mapRow sera automatiquement appelé par le jdbcTemplate
+	// pour chaque ligne revnoyée par la requette executée
+	// le jdbcTemplate se charge du reste, le parcours du resultSet
+	// et le remplissage d'une List avec les produits renvoyés
+	private static class ProduitMapper implements RowMapper<Produit> {
+
+		@Override
+		public Produit mapRow(ResultSet rs, int pos) throws SQLException {
+			return new Produit(rs.getInt("id"),
+								rs.getString("nom"),
+								rs.getDouble("prix"),
+								rs.getDouble("poids"));
+		}
+	}
+	
+	
+	public static final String FIND_ALL_SQL =
+			"select id, nom, prix, poids from produit";
+	public static final String FIND_BY_ID_SQL =
+			"select id, nom, prix, poids from produit where id=?";
+	public static final String UPDATE_SQL = 
+			"update produit set nom=?, prix=?, poids=? where id=?";
+	public static final String INSERT_SQL = 
+			"insert into produit (nom, prix, poids) VALUES (?, ?, ?)";
+	
+	
+	private JdbcTemplate jdbcTemplate;
+	public JdbcTemplate getJdbcTemplate() {return jdbcTemplate;}
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {this.jdbcTemplate = jdbcTemplate;}
+	
+	public List<Produit> findAll() {
+		return jdbcTemplate.query(FIND_ALL_SQL, new Object[]{}, new ProduitMapper());
+	}
+	
+	public Produit findByID(int id) {
+		return jdbcTemplate.queryForObject(FIND_BY_ID_SQL,
+											new Object[]{id},
+											new ProduitMapper());
+	}
+	
+	public int save(Produit p) {
+		if (p.getId() == 0)
+			return jdbcTemplate.update(INSERT_SQL,
+					p.getNom(), p.getPrix(), p.getPoids());
+		else
+			return jdbcTemplate.update(UPDATE_SQL,
+					p.getNom(), p.getPrix(), p.getPoids(), p.getId());
+	}
+
+}
